1e7983b433543a1e6ebedb159958f2d696c31cab
Rutong Pei <123343365+RutongPei@users.noreply.github.com>
2026-04-19 15:04:59 -0400

    Add BE/BE_re columns, LAMBDAF support, fix read_input_file symbol

--- src/comenv_lambda.f90
+++ src/comenv_lambda.f90
@@ -1,6 +1,4 @@
 subroutine comenv_lambda(KW,M0,L,R,MENVD,LAMBDA,id,LAMBF)
-
-    ! get appropiate ZAMS radius and calculate common envelope lambda
     use track_support
     implicit none
     real(dp), intent(in):: M0,L,R,MENVD,LAMBDA
@@ -14,7 +12,6 @@ subroutine comenv_lambda(KW,M0,L,R,MENVD,LAMBDA,id,LAMBF)
     integer :: idd
     type(track), pointer :: t
 
-
     idd = 1
     if(present(id)) idd = id
 
@@ -27,13 +24,28 @@ subroutine comenv_lambda(KW,M0,L,R,MENVD,LAMBDA,id,LAMBF)
     else
         RZAMS = 10.d0**t% tr(i_logR,ZAMS_EEP)
     endif
-    
-    if (i_binding_energy > 0) then
-        LAMBF = - (t% pars% mass * (t% pars% mass - t% pars% core_mass)) / (t% pars% binding_energy * R)
+
+    if (LAMBDA < -1.5d0 .and. i_binding_energy_re > 0) then
+        LAMBF = - (t%pars%mass * (t%pars%mass - t%pars%core_mass)) / &
+         ((t%pars%binding_energy_re / 3.8d48) * R)
+        write(0,'(A,I3,A,F8.3,A,F8.3,A,F8.3,A,ES12.4,A,ES12.4,A,F10.5)') &
+            '[CE lambda] KW=',KW,' M=',t%pars%mass,' Mc=',t%pars%core_mass, &
+            ' R=',R,' BE_re=',t%pars%binding_energy_re, &
+            ' BE=',t%pars%binding_energy,' LAMBF(be_re)=',LAMBF
+    else if (LAMBDA < -0.5d0 .and. i_binding_energy > 0) then
+        LAMBF = - (t%pars%mass * (t%pars%mass - t%pars%core_mass)) / &
+         ((t%pars%binding_energy / 3.8d48) * R)
+        write(0,'(A,I3,A,F8.3,A,F8.3,A,F8.3,A,ES12.4,A,F10.5)') &
+            '[CE lambda] KW=',KW,' M=',t%pars%mass,' Mc=',t%pars%core_mass, &
+            ' R=',R,' BE=',t%pars%binding_energy,' LAMBF(be)=',LAMBF
     else
         LAMBF = CELAMF(KW,M0,L,R,RZAMS,MENVD,LAMBDA)
+        write(0,'(A,I3,A,F8.3,A,F8.3,A,F10.5)') &
+            '[CE lambda] KW=',KW,' M=',M0,' R=',R, &
+            ' RZAMS=',RZAMS,' LAMBF(analytic)=',LAMBF
     endif
-    !comenv_lambda = LAMBF
+
+    LAMBF = MIN(100.0d0, LAMBF)
 
     nullify(t)
 end subroutine comenv_lambda
